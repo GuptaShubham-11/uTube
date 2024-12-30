ced87c2e7b12519cc41e1e859ccda8f3900513a4
GuptaShubham-11 <shu919847@gmail.com>
2024-12-30 09:07:52 +0530

    add utils code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 
 dotenv.config({
-    path: "./env"
-})
+    path: "./.env"
+});
+
 connectDB()
+.then(() => {
+    app.on("Error", () => {
+        console.log("UNEXPECTRD ERROR: ", error);
+        throw error;
+    });
+
+    app.listen( process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+})
+.catch( (error) => {
+    console.log(`MONOGO DB connection error: ${error}`);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,51 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch((error) => (next(error)));
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Another way to write upper wrapper code 
+// const asyncHandler = (fx) => async (req, res, next) => {
+//     try {
+//         await fx(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// }
