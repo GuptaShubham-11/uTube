4aa77440f9be0aa67d2467e42172737276b1995d
GuptaShubham-11 <shu919847@gmail.com>
2025-01-02 05:06:10 +0530

    add register user controller & bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,71 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinry } from "../utils/cloudinry.js"
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "Shubham"
+
+    // Get user data from frontend
+    const { username, email, fullname, password } = req.body;
+
+    // Validation for data
+    if (
+        [ username, email, fullname, password].some((field) => (field?.trim() === ""))
+    ) {
+        throw new ApiError(400, "All fields are required!!!");
+        
+    }
+
+    // Check if user already exists
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
     });
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already existed.");
+    }
+    
+    // Get images local path from multer
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    // Now upload the local images to coludinry platform
+    const avatar = await uploadOnCloudinry(avatarLocalPath);
+    const coverImage = await uploadOnCloudinry(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    // Data store in db
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    });
+
+    // check User created successfully or not
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    if (!createdUser) {
+        throw new ApiError(500, "User not register properly!!!");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully.")
+    );
+
 });
 
 export { registerUser };
