34e7e1faf965646c48da3c2ed4a287505888d507
GuptaShubham-11 <shu919847@gmail.com>
2025-01-02 21:03:11 +0530

    add login user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,36 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinry } from "../utils/cloudinry.js"
 
 
-const registerUser = asyncHandler( async (req, res) => {
+const genrateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+
+        const accessToken = user.genrateAccessToken();
+        const refreshToken = user.genrateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(500, "Srever error because access and refresh tokens are not genrated properly");
+    }
+}
+
+const registerUser = asyncHandler(async (req, res) => {
 
     // Get user data from frontend
     const { username, email, fullname, password } = req.body;
 
     // Validation for data
     if (
-        [ username, email, fullname, password].some((field) => (field?.trim() === ""))
+        [username, email, fullname, password].some((field) => (field?.trim() === ""))
     ) {
-        throw new ApiError(400, "All fields are required!!!");    
+        throw new ApiError(400, "All fields are required!!!");
     }
 
     // Check if user already exists
@@ -25,7 +41,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already existed.");
     }
-    
+
     // Get images local path from multer
     const avatarLocalPath = req.files?.avatar[0]?.path;
     // const coverImageLocalPath = req.files?.coverImage[0]?.path; Because it give us to undifine error
@@ -33,7 +49,7 @@ const registerUser = asyncHandler( async (req, res) => {
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path;
-    }    
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
@@ -42,7 +58,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // Now upload the local images to coludinry platform
     const avatar = await uploadOnCloudinry(avatarLocalPath);
     const coverImage = await uploadOnCloudinry(coverImageLocalPath);
-    
+
     if (!avatar) {
         throw new ApiError(400, "Avatar file is required");
     }
@@ -72,4 +88,55 @@ const registerUser = asyncHandler( async (req, res) => {
 
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+    // Take data from req
+    const { username, email, password } = req.body;
+
+    // Validation on email || username is required
+    if (!username || !email) {
+        throw new ApiError(400, "Username or email is required");
+    }
+
+    // Check in database
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    });
+
+    // Not exist
+    if (!user) {
+        throw new ApiError(409, "User does not exist!");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Password does not correct!");
+    }
+
+    const { accessToken, refreshToken } = await genrateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password, -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(201)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, 
+                accessToken, 
+                refreshToken
+            },
+            "User logged In successfully"
+        )
+    )
+});
+
+export { registerUser, loginUser };
