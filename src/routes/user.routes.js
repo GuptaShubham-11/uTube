2c7f0a742a9ee9f8adb72f9b622e6f3a48472134
GuptaShubham-11 <shu919847@gmail.com>
2025-01-05 16:43:37 +0530

    Add multipule routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser,
+    updateAcountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middelware.js"
 
@@ -24,5 +34,11 @@ router.route("/login").post(loginUser)
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-user-details").post(verifyJWT, updateAcountDetails)
+router.route("/update-user-avatar").post(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-user-cover").post(verifyJWT, updateUserCoverImage)
+
 
 export default router;
