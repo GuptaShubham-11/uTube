208006a50cb57077d313d7bdf30552d18a419ed3
GuptaShubham-11 <shu919847@gmail.com>
2025-02-13 15:58:02 +0530

    add theme management with toggle functionality and create Home and NotFound pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { HeroSection, Features } from "../components";
+
+export default function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <Features />
+        </div>
+    );
+}
