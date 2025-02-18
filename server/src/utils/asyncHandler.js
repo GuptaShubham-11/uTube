a4b90193fdb64b0735d2b0c7f2f357f0d53f9b4a
GuptaShubham-11 <shu919847@gmail.com>
2025-02-18 10:49:49 +0530

    refactor: update asyncHandler to improve error handling and simplify syntax

--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,21 +1,26 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error),
-    );
-  };
+// First way to handle 
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+//       next(error),
+//     );
+//   };
+// };
+
+// export { asyncHandler };
+
+
+// Second way to handle
+const asyncHandler = (fx) => async (req, res, next) => {
+  try {
+    await fx(req, res, next);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
 };
 
 export { asyncHandler };
 
-// Another way to write upper wrapper code
-// const asyncHandler = (fx) => async (req, res, next) => {
-//     try {
-//         await fx(req, res, next);
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message
-//         });
-//     }
-// }
