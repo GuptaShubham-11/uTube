419cbafb5e604181d1757ebab11b139db70608dc
GuptaShubham-11 <shu919847@gmail.com>
2025-02-18 11:39:00 +0530

    refactor: adjust padding in Features component for improved layout consistency

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -6,16 +6,13 @@ const features = [
 
 export default function Features() {
   return (
-    <section className="py-16 px-4 bg-background-light dark:bg-background-dark">
+    <section className="py-5 px-4 bg-background-light dark:bg-background-dark">
       <div className="max-w-screen-xl mx-auto">
-        <h2 className="text-3xl font-semibold text-center text-primary-light dark:text-primary-dark mb-10">
-          Key Features
-        </h2>
         <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {features.map((feature, index) => (
             <div
               key={index}
-              className="p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 bg-background-light dark:bg-background-dark border border-secondary-light dark:border-secondary-dark hover:bg-accent-light dark:hover:bg-accent-dark hover:text-text-light dark:hover:text-text-dark"
+              className="p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 bg-background-light dark:bg-background-dark border border-secondary-light dark:border-secondary-dark"
             >
               <h3 className="text-2xl font-semibold text-primary-light dark:text-primary-dark mb-4">
                 {feature.title}
