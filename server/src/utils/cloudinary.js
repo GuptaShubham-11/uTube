b6fd0aff68fe3c449cb48629c42d2c10f89b3ad3
GuptaShubham-11 <shu919847@gmail.com>
2025-02-28 07:40:29 +0530

    fix: standardize quotation marks and improve code formatting in upload and Cloudinary utility

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,17 +1,17 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath, resource_type = "auto") => {
   try {
     if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: resource_type
+      resource_type: resource_type,
     });
     fs.unlinkSync(localFilePath);
     return response;
@@ -20,12 +20,12 @@ const uploadOnCloudinary = async (localFilePath, resource_type = "auto") => {
     fs.unlinkSync(localFilePath);
     return null;
   }
-}
+};
 
 const extractPublicId = (url) => {
-  const parts = url.split('/');
+  const parts = url.split("/");
   const fileName = parts.pop(); // Get the file name with extension
-  const publicId = fileName.split('.')[0]; // Remove the file extension
+  const publicId = fileName.split(".")[0]; // Remove the file extension
   return publicId; // Return only the public ID
 };
 
@@ -33,10 +33,10 @@ const deleteOnCloudinary = async (filePath, resource_type = "image") => {
   try {
     if (!filePath) return null;
 
-    // Extract public ID        
+    // Extract public ID
     const publicId = extractPublicId(filePath);
 
-    // Delete from Cloudinary        
+    // Delete from Cloudinary
     const response = await cloudinary.uploader.destroy(publicId, {
       resource_type: resource_type,
     });
