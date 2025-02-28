a231b8a113838830950fbbc4852b5473e4ee1f7f
GuptaShubham-11 <shu919847@gmail.com>
2025-02-28 08:02:04 +0530

    fix: update multer middleware to use path module for temporary directory

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,8 +1,11 @@
 import multer from "multer";
+import path from "path";
+
+const tempDir = path.join(process.cwd(), "server/public/temp");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
